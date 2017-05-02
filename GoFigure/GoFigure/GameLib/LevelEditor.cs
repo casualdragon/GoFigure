f9b4b6d494000a35fe00f7e2748cc4de708ef22b
Redtortuga <cmorgan20@my.apsu.edu>
2017-05-02 09:42:39 -0500

    LevelEditor

--- GoFigure/GoFigure/GameLib/LevelEditor.cs
+++ GoFigure/GoFigure/GameLib/LevelEditor.cs
@@ -1,10 +1,100 @@
 ﻿//------------------------------------------------------------------------------
 // Used to create a place where the user can generate their own levels.
 //------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
 
-public class LevelEditor
+public class LevelEditor : Microsoft.Xna.Framework.Game
 {
-	public virtual Level level
+    GraphicsDeviceManager graphics;
+    //Allows png pictures to be drawn using the same settings
+    public static SpriteBatch batch;
+    public static Vector2 clientBounds;
+
+    //Declare the GUI
+    GoFigure.LevelEditor.HUD hud;
+
+    public LevelEditor()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+    }
+
+    protected override void Initialize()
+    {
+        //Makes the mouse visibile
+        this.IsMouseVisible = true;
+
+        //Sets the window size
+        graphics.PreferredBackBufferHeight = 700;
+        graphics.PreferredBackBufferWidth = 800;
+        graphics.ApplyChanges();
+
+        //Sets the bound of client
+        clientBounds = new Vector2(Window.ClientBounds.Width, Window.ClientBounds.Height);
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        //Creates a new Spritebatch used to draw textures;
+        batch = new SpriteBatch(GraphicsDevice);
+
+        //Initializes the HUD
+        hud = new GoFigure.LevelEditor.HUD(Content);
+    }
+
+    protected override void UnloadContent()
+    {
+        base.UnloadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        //Exits the LevelEditor
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+        {
+            this.Exit();
+        }
+        hud.Update();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.DarkRed);
+
+        //Starts SpriteBatch call
+        batch.Begin();
+
+        //Draw the Hud
+        hud.Draw();
+
+        base.Draw(gameTime);
+
+        batch.End();
+    }
+
+    /*
+    public LevelEditor(Level level)
+    {
+        this.level = level;
+        game = new Game(level, true);
+    }
+
+    public virtual Level level
 	{
 		get;
 		set;
@@ -35,12 +125,11 @@ public class LevelEditor
         //}
         return true;
 	}
+    */
 
-	public LevelEditor(Level level)
-	{
-        this.level = level;
-        game = new Game(level, true);
-	}
+
+
+
 
 
 }
---
+++ GoFigure/GoFigure/LevelEditor/Button.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GoFigure.LevelEditor
+{
+    public class Button
+    {
+        public bool clicked = false;
+        public bool prevClick = false;
+        bool hover = false;
+        public Vector2 position;
+        Texture2D texture;
+        Rectangle colBox;
+
+        public Button(Texture2D tex, Vector2 position)
+        {
+            //Constructor updating instance variables
+            this.texture = tex;
+            this.position = position;
+
+        }
+
+        public virtual void Update()
+        {
+            colBox = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+
+            //Finds the current mouse position
+            MouseState mouse = Mouse.GetState();
+            Rectangle mousePosition = new Rectangle(mouse.X, mouse.Y, 1, 1);
+
+            //Finds out if the user's mouse is hovering over button
+            if (mousePosition.Intersects(colBox))
+            {
+                hover = true;
+            } else
+            {
+                hover = false;
+            }
+
+            //Finds out if button has been left clicked and it wasn't previous click
+            if (mousePosition.Intersects(colBox) && mouse.LeftButton == ButtonState.Pressed && !prevClick)
+            {
+                clicked = true;
+            } else
+            {
+                clicked = false;
+            }
+
+            //clicked saved as prevClick
+            prevClick = prevClick || clicked;
+        }
+
+        public virtual void Effect()
+        {
+            //Is overriden by other buttons
+        }
+
+        public void Draw()
+        {
+            //Drawing of button
+            if (hover)
+            {
+                LevelEditor.batch.Draw(texture, position, Color.White);
+                LevelEditor.batch.Draw(texture, position, new Color(255, 0, 0, 180));
+            } else
+            {
+                LevelEditor.batch.Draw(texture, position, Color.White);
+            }
+        }
+    }
+}
